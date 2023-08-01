17ea26ab8219b818ccee14f217f1930f8f878940
Twincki <selutin.a@mail.ru>
2023-08-01 11:32:06 +0300

    созданы стили для скроллбара

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="light">
+  <div className="dark">
     <App />
   </div>
 );
